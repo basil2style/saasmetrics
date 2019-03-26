8e7b0fa9dffd30ee69175ca794a5ab1b68eabcab
Basil <basiltalias92@gmail.com>
2019-03-26 12:00:13 -0400

    Added the values

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -25,25 +25,43 @@ export class DataService {
     },
     {
       itemName: 'Customer Lifetime Value',
+      value1: 'Churn Rate',
+      value2: 'Total Revenue',
+      value3: 'Customers',
       'learnUrl': 'https://saasmetrics.co/customer-lifetime-value/'
     },
     {
       itemName: 'Customer Acquisition Cost',
+      value1: 'Marketing Expenses',
+      value2: 'Sales Expenses',
+      value3: 'New Customers',
       'learnUrl': 'https://saasmetrics.co/customer-acquisition-cost/'
     },
     {
       itemName: 'Months to Cash Out',
+      value1: 'Initial Cash',
+      value2: 'Monthly Operating Expenses',
+      value3: 'Monthly Revenue',
       'learnUrl': 'https://www.liveplan.com/blog/metrics-in-a-minute-cash-burn-rate/'
     },
     {
       itemName: 'Marketing ROI',
+      value1: 'Gross Profit',
+      value2: 'Marketing Investment',
       'learnUrl': 'http://www.marketingmo.com/campaigns-execution/how-to-calculate-roi-return-on-investment/'
     },
     {
       itemName: 'Average Revenue per Account',
+      value1: 'Total Revenue',
+      value2: 'Paying Customers',
       'learnUrl': 'https://saasmetrics.co/average-revenue-per-account/'
     },
-    { itemName: 'New User Viral Coefficient' }
+    {
+      itemName: 'New User Viral Coefficient',
+      value1: 'Current Users',
+      value2: 'Invitations Sent by Users',
+      value3: 'Conversion Rates',
+    }
   ]
 
   //pass data
--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -10,8 +10,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class InfoPage implements OnInit {
   data: any;
   title: string;
-  value1: string = "Value1"
-  value2: string = "Value2"
+  value1: string;
+  value2: string;
+  value3: string;
+  value4: string;
+  value5: string;
+  value6: string;
+  value7: string;
+
   constructor(private route: ActivatedRoute, private router: Router) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
@@ -22,7 +28,14 @@ export class InfoPage implements OnInit {
   }
 
   ngOnInit() {
-   this.title = this.data.itemName
+    this.title = this.data.itemName
+    this.value1 = this.data.value1;
+    this.value2 = this.data.value2;
+    this.value3 = this.data.value3;
+    this.value4 = this.data.value4;
+    this.value5 = this.data.value5;
+    this.value6 = this.data.value6;
+    this.value7 = this.data.value7;
   }
 
 }
